0943b870c8be05e9f3e9ae9d6f9653bca4177b3e
mirparx-ops <mirko.parente@gmail.com>
2026-03-14 08:29:29 +0100

    My Django Girls app, first comit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w#mo!c_w6vwozk#u^(64e245qr&sf)9n5h0+!d9orn6w_)auu@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'mirparx.pythonanywhere.com']
 
 
 # Application definition
