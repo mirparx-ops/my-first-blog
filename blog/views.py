061f743c7cfae0179d58643ba344a83e300e45b4
mirparx-ops <mirko.parente@gmail.com>
2026-03-16 14:47:12 +0100

    debugging

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
+def post_list_2(request):
+    return render(request, 'blog/post_list_2.html', {})
+
 # Create your views here.
