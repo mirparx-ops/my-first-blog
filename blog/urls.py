86e1434097e2f3d0d3db4ac599b4ca80dfa87fca
mirparx-ops <mirko.parente@gmail.com>
2026-03-19 22:18:45 +0100

    tutorial 1 finito

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_details, name='post_details')
+    path('post/<int:pk>/', views.post_details, name='post_details'),
+    path('post/new/',views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     # path('pl/', views.post_list_2, name='post_list_2'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render,get_object_or_404
-
+from django.shortcuts import render,get_object_or_404, redirect
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -16,5 +16,35 @@ def post_list_2(request):
 def post_details(request,pk):
     post= get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_details.html', {'post':post})
+
+def post_new(request):
+    
+    if request.method =="POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save( commit = False )
+            post.author =  request.user
+            post.published_date = timezone.now()
+            post.save()
+            #return redirect('post_details', pk=post.pk)
+            return redirect('post_list')
+    else:       
+        form = PostForm()
+    return render(request,'blog/post_edit.html', {'form': form})
+
+def post_edit(request,pk):
+    post =get_object_or_404(Post, pk=pk)
+    if request.method =="POST":
+        form =  PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author=request.user
+            post.plublished_date = timezone.now()
+            post.save()
+            #return redirect('post_details',pk=post.pk)
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request,'blog/post_edit.html',{'form' : form})
 
 # Create your views here.
