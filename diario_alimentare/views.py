51aaf654160528e6b2ea12b50fdd3dd885309272
mirparx-ops <mirko.parente@gmail.com>
2026-03-20 09:33:25 +0100

    update diario

--- diario_alimentare/views.py
+++ diario_alimentare/views.py
@@ -4,8 +4,11 @@ from django.utils import timezone
 
 
 def diario_list(request):
-    
-    pasti = Pasto.objects.all().order_by('ora')
+    data_selezionata = request.GET.get('data')
+
+    if data_selezionata:
+        pasti = Pasto.objects.filter(ora__date=data_selezionata)
+    else:
+        pasti = Pasto.objects.all().order_by('ora')
     # posts = Post.objects.all().order_by('-published_date')
     return render(request, 'diario/diario_list.html', {'pasti':pasti})
-
